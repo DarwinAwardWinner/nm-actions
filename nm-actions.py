49f8837c1c3cc238812bbadd2133fe13f2aab84f
Ryan C. Thompson <rct@thompsonclan.org>
2011-01-10 12:43:53 -0800

    Initial working version.

---
+++ nm-actions.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import dbus
+import gobject
+
+class NetworkManagerStateChangeHandler(object):
+    from dbus.mainloop.glib import DBusGMainLoop
+    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+    bus = dbus.SystemBus()
+    proxy = bus.get_object('org.freedesktop.NetworkManager',
+                           '/org/freedesktop/NetworkManager',)
+    iface = dbus.Interface(proxy, 'org.freedesktop.NetworkManager')
+
+    def __init__(self, connect_handler=None, disconnect_handler=None):
+        self.connect_handler = connect_handler
+        self.disconnect_handler = disconnect_handler
+        self.enable()
+
+    def enable(self):
+        # This will guarantee that an action is run immediately.
+        self.last_connection_status = not self.connected()
+        self.handle_state_change()
+        NetworkManagerStateChangeHandler.iface.connect_to_signal('StateChanged', self.handle_state_change)
+
+    def connected(self):
+        """Returns True if current state is connected, else False."""
+        return NetworkManagerStateChangeHandler.iface.state() == 3
+
+    def handle_state_change(self, *args):
+        """If Network Manager has just connected or disconnected, call
+        the appropriate handler."""
+        new_connection_status = self.connected()
+        if new_connection_status != self.last_connection_status:
+            if new_connection_status:
+                self.connect_handler()
+            else:
+                self.disconnect_handler()
+        self.last_connection_status = new_connection_status
+
+if __name__ == '__main__':
+    def connect_handler():
+        print "Connected"
+    def disconnect_handler():
+        print "Disconnected"
+    handler = NetworkManagerStateChangeHandler(connect_handler, disconnect_handler)
+    loop = gobject.MainLoop()
+    loop.run()
